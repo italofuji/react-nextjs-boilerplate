8c87ebb9d0064ea5d7323291b0274ecdc520d76e
Italo Y. Fujisawa <italofuji@gmail.com>
2020-11-07 16:20:25 -0300

    Add support to husky and lint-staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 
 export default function Home() {
+  const test = 'teste'
+
   return (
     <div className={styles.container}>
       <Head>
